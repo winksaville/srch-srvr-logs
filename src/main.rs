1dfdcd736c8d00379d9a575b173a27371b3f7b79
Wink Saville <wink@saville.com>
2022-09-02 20:03:19 -0700

    wip: Start attempt2
    --ssh-dest works but if not supplied a todo! error:
    
     $ cargo run "Starting\|version:\|ERROR" --since=-1d
       Compiling srch-srvr-logs v0.1.0 (/home/wink/prgs/rust/myrepos/srch-srvr-logs)
        Finished dev [unoptimized + debuginfo] target(s) in 0.46s
         Running `/home/wink/prgs/rust/myrepos/srch-srvr-logs/target/debug/srch-srvr-logs 'Starting\|version:\|ERROR' --since=-1d`
     Process eth1.service:
     thread 'main' panicked at 'not yet implemented: Local execution isn't supported yet :)', src/main.rs:53:13

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,19 @@ use std::process::Command;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
+#[rustfmt::skip]
 struct Args {
-    #[clap(value_parser, help = "ssh login, account@machine")]
-    acct_name: String,
-
-    #[clap(
-        value_parser,
-        help = "rust regex string, an empty string, \"\", matches everything"
-    )]
+    #[clap(value_parser, help = "rust regex string, an empty string, \"\", matches everything")]
     regex: String,
 
-    #[clap(
-        short = 't',
-        long,
-        value_parser,
-        default_value = "-1d",
-        help = "journalctl --since parameter but use '=', Example: --since=-1h"
-    )]
+    #[clap(short, long, value_parser, help = "ssh dest, where dest is user@machine")]
+    ssh_dest: Option<String>,
+
+    #[clap(long, value_parser, default_value = "-1d",
+            help = "journalctl --since parameter but use '=', Example: --since=-1h")]
     since: String,
 
-    #[clap(short, long, value_parser, multiple = true, default_values = &["eth1", "beacon-chain", "validator"] )]
+    #[clap(long, value_parser, multiple = true, default_values = &["eth1", "beacon-chain", "validator"] )]
     services: Vec<String>,
 }
 
@@ -37,53 +30,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let srvc_str = srvc + ".service";
         println!("Process {srvc_str}:");
 
-        // Create the command
-        let mut cmd = Command::new("ssh");
+        // Get the regex to match
         let regex = if args.regex.is_empty() {
+            // It's empty so specfically apss "" so there is something
             "\"\""
         } else {
+            //
             &args.regex
         };
-        cmd.args([
-            &args.acct_name,
-            "journalctl",
-            "-u",
-            &srvc_str,
-            "--since",
-            &args.since,
-            "|",
-            "rg",
-            "--color",
-            "always",
-            "-e",
-            regex,
-        ]);
-        //println!("cmd: {:#?}",cmd);
 
-        // Execute the command
-        let result = cmd.output()?;
+        // Check of we have a ssh_dest
+        let rg_result = if let Some(ref dest) = args.ssh_dest {
+            // Yes, so ssh to ssh_dest and use journalctl
+            let mut cmd = Command::new("ssh");
+            cmd.arg(dest);
+            cmd.args(["journalctl", "-u", &srvc_str, "--since", &args.since]);
+            cmd.arg("|");
+            cmd.args(["rg", "--color", "always", "-e", regex]);
+            cmd.output()?
+        } else {
+            // Maybe use https://github.com/hniksic/rust-subprocess
+            // and more specifically probably pipelines:
+            //   https://github.com/hniksic/rust-subprocess#pipelines
+            todo!("Local execution isn't supported yet :)");
+        };
 
-        let code = result.status.code();
-        const ONE: i32 = 1;
+        let code = rg_result.status.code();
+        const EXIT_CODE_ZERO: i32 = 0;
+        const EXIT_CODE_ONE: i32 = 1;
         match code {
-            Some(0) => {
-                let stdout = String::from_utf8_lossy(&result.stdout);
+            Some(EXIT_CODE_ZERO) => {
+                let stdout = String::from_utf8_lossy(&rg_result.stdout);
                 println!("{stdout}");
             }
-            Some(ONE) => {
-                let stderr = String::from_utf8_lossy(&result.stderr);
+            Some(EXIT_CODE_ONE) => {
+                let stderr = String::from_utf8_lossy(&rg_result.stderr);
                 if stderr.len() == 0 {
                     println!(" <no matches for regex: \"{}>\"", args.regex);
                 } else {
-                    println!("Exit code: {ONE}, {stderr}");
+                    println!("Exit code: {EXIT_CODE_ONE}, {stderr}");
                 }
             }
             Some(v) => {
                 // All other errors
-                let stderr = String::from_utf8_lossy(&result.stderr);
+                let stderr = String::from_utf8_lossy(&rg_result.stderr);
                 return Err(format!("Exit code: {v}, {stderr}").into());
             }
-
             None => {
                 return Err("No exit code, done".into());
             }
